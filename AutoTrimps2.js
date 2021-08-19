296802ad1dbb776c606b2b90e3b5372f0f097df4
sharsnik <sharsnik@gmail.com>
2021-08-19 01:24:59 -0400

    Update AutoTrimps2.js

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -318,7 +318,7 @@ function mainLoop() {
         var isVoid = getCurrentMapObject().location == "Void";
         //REquality
         if (getPageSetting('Rminequalityfactor') > 0) {
-            if (game.global.soldierHealthMax * (1 + getEnergyShieldMult()) < Math.max(getCurrentWorldCell().attack, getCurrentMapCell().attack) * game.portal.Equality.getMult() * getPageSetting('Rminequalityfactor') * (isVoid ? 2 : 1)) {
+            if (game.global.soldierHealthMax/* * (1 + getEnergyShieldMult())*/ < Math.max(getCurrentWorldCell().attack, getCurrentMapCell().attack) * game.portal.Equality.getMult() * getPageSetting('Rminequalityfactor') * (isVoid ? 2 : 1)) {
                 game.portal.Equality.scalingActive = false;
                 game.portal.Equality.disabledStackCount = game.portal.Equality.disabledStackCount + 1;
 
@@ -329,7 +329,7 @@ function mainLoop() {
             document.getElementById('equalityA').innerHTML = "Equality (" + game.portal.Equality.disabledStackCount + ")";
         }
         if (getPageSetting('Rmaxequalityfactor') > 0) {
-            if (game.global.soldierHealthMax * (1 + getEnergyShieldMult()) > Math.max(getCurrentWorldCell().attack, getCurrentMapCell().attack) * game.portal.Equality.getMult() * getPageSetting('Rmaxequalityfactor') * (isVoid ? 2 : 1)) {
+            if (game.global.soldierHealthMax/* * (1 + getEnergyShieldMult())*/ > Math.max(getCurrentWorldCell().attack, getCurrentMapCell().attack) * game.portal.Equality.getMult() * getPageSetting('Rmaxequalityfactor') * (isVoid ? 2 : 1)) {
                 game.portal.Equality.scalingActive = false;
                 game.portal.Equality.disabledStackCount = game.portal.Equality.disabledStackCount - 1;
 
